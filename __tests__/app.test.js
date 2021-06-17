fc5d3564bc2623d2f2d5213c4f3a5e6dbcc5b012
keven-ruiz <kevenruizh@gmail.com>
2021-06-17 11:54:29 -0700

    Wrote a test that would fail, creating my pathing next

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable space-before-function-paren */
 const request = require('supertest');
 const app = require('../lib/app');
+const fsPromise = require('fs/promises');
 
 describe('app routes', () => {
 
@@ -10,6 +11,20 @@ describe('app routes', () => {
 
     expect(res.text).toEqual('hello there');
     expect(res.type).toEqual('text/html');
+  });
+
+
+
+});
+
+describe('promise routes', () => {
+
+  it('public folder index.html file', async () => {
+    const res = await request(app).get('/index.html');
+    const expected = await fsPromise.readFile('public/index.html', 'utf-8');
+
+    expect(res.status).toEqual(200);
+    expect(res.text).toEqual(expected);
   });
 
 });
